3c809780599da0832010eb2eae35e03249316de9
Ceganjac <ceganjacaleksandar@gmail.com>
2026-03-08 22:49:32 +0100

    Početni prozori

--- ZdravstvenaUstanova/src/main/MainClass.java
+++ ZdravstvenaUstanova/src/main/MainClass.java
@@ -1,12 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
 
+import gui.forme.PrijavaPanel;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
- * @author Aleksandar
+ * @author Aleksandar Čeganjac
  */
 public class MainClass {
 
@@ -14,7 +19,20 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        SwingUtilities.invokeLater(() -> {
+            // panel
+            PrijavaPanel panel = new PrijavaPanel();
+
+            // dialog
+            JDialog dialog = new JDialog((JFrame) null, "Пријава", true);
+            dialog.add(panel);
+            dialog.pack();
+            dialog.setLocationRelativeTo(null);
+            dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+            dialog.setVisible(true);
+        });
+
     }
-    
+
 }
